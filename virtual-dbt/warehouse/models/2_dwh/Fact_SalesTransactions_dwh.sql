45dc143f67695ee4ba6cae835dc93b114b2a20c1
Alexandern1988 <107794923+Alexandern1988@users.noreply.github.com>
2023-06-26 14:56:05 +0300

    Develop Fact Daily Periodical and corrections to Fact Sales Transactions

--- virtual-dbt/warehouse/models/2_dwh/Fact_SalesTransactions_dwh.sql
+++ virtual-dbt/warehouse/models/2_dwh/Fact_SalesTransactions_dwh.sql
@@ -20,12 +20,12 @@ select
 	,ct.CustomerTransactionID as TransactionID
 	,ct.AmountExcludingTax
 	,ct.TaxAmount
-	,TransactionAmount
-	,icl.total_lineProfit as TransactionProfit
+	,ct.TransactionAmount
+	,lpa.total_lineProfit as TransactionProfit
 from dev.dimDate_dwh as dd 
 	join {{ ref('customerTransactions_stg') }} as ct on convert(date, dd.DateKey, 120) = ct.TransactionDate
-	left join {{ ref('dimInvoice_dwh') }} as dic on ct.CustomerID = dic.CustomerID
-	left join {{ ref('dimPeople_dwh') }} as dp on dic.PrimaryContactPersonID = dp.PeopleKey
-	left join {{ ref('dimCustomer_dwh') }} as dc on ct.CustomerID = dc.CustomerID and dc.CurrentFlag = 1
-	left join {{ ref('dimPayment_dwh') }} as dpy on dpy.PaymentKey = ct.PaymentMethodID
-	left join lineProfit_agg as icl on dic.InvoiceKey = icl.InvoiceID
+	left join {{ ref('dimInvoice_dwh') }}      as dic on dic.InvoiceKey = ct.InvoiceID
+	left join {{ ref('dimPeople_dwh') }}       as dp on dic.PrimaryContactPersonID = dp.PeopleKey
+	left join {{ ref('dimCustomer_dwh') }}     as dc on ct.CustomerID = dc.CustomerID and dc.CurrentFlag = 1
+	left join {{ ref('dimPayment_dwh') }}      as dpy on dpy.PaymentKey = ct.PaymentMethodID
+	left join lineProfit_agg                   as lpa on dic.InvoiceKey = lpa.InvoiceID
